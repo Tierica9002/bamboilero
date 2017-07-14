5b88902b60d76783827f26d45e907815160c6dbb
Andrei Sorescu <and.sorescu@gmail.com>
2017-07-14 13:32:51 +0300

    React, babel, jsx

--- webpack.config.js
+++ webpack.config.js
@@ -15,8 +15,17 @@ module.exports = {
                     'css-loader',
                     'sass-loader'
                 ]
+            },
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                    presets: ['env', 'react-app']
+                    }
+                }
             }
-
         ]
     }    
 }
