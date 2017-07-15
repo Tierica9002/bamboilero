781e36b250d6a8b192ac8698dd0739fe83d4b79e
Andrei Sorescu <and.sorescu@gmail.com>
2017-07-15 03:53:40 +0300

    Added sample app component

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+class App extends Component {
+  constructor(props) {
+      super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>test hmrxxssdddd</h1>
+        <input type="text" />
+      </div>
+    )
+  }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import App from './components/App';
 import './style.scss';
+
 ReactDOM.render(
-   <h1>Working webpack-dev-server</h1>,
-    document.getElementById('root'));
+  <App />,
+  document.getElementById('root'));
