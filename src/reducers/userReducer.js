a474ee1e7ec368f5b9366bb9aefaea86f2470cc4
Andrei Sorescu <and.sorescu@gmail.com>
2017-07-16 20:46:55 +0300

    Added reducer & store boilerplate

---
+++ src/reducers/userReducer.js
@@ -0,0 +1,10 @@
+export default function userReducer(state = [], action) {
+    switch (action.type) {
+        case 'GET_USERS':
+            return [
+                ...action.users
+            ];
+        default:
+            return state;
+    }
+}
